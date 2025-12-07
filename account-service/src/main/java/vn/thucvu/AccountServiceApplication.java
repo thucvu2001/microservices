ffc274e37b9e3ffc87ae64de6afa85580eec992b
thucvu2001 <vuthuc3152001@gmail.com>
2025-12-07 11:41:34 +0700

    first commit microservices

--- account-service/src/main/java/vn/thucvu/AccountServiceApplication.java
+++ account-service/src/main/java/vn/thucvu/AccountServiceApplication.java
@@ -3,11 +3,15 @@ package vn.thucvu;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class AccountServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AccountServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+        System.out.println("Default Timezone: " + TimeZone.getDefault().getID());
+        SpringApplication.run(AccountServiceApplication.class, args);
+    }
 
 }
